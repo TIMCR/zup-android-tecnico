c8181376654fac362e15fe11dbec2efc030479e3
João Paulo Polles <joaopaulopolles@gmail.com>
2018-04-24 20:11:48 -0300

    configurado para funcionar com o VcMCR

---
+++ src/vcmcr/java/com/lfdb/zuptecnico/config/Constants.java
@@ -0,0 +1,12 @@
+package com.lfdb.zuptecnico.config;
+
+
+public class Constants {
+    public static final String API_URL = "http://vc.mcr.pr.gov.br:8282";
+    public static final String PLACES_KEY = "AIzaSyCvAATVMzlV9XQDD8ufp3IK4EvCxAGjlPY";
+
+    public static final double DEFAULT_LAT = -24.5581316;
+    public static final double DEFAULT_LON = -54.057559;
+
+    public static final int MAP_DEFAULT_ZOOM = 14;
+}
